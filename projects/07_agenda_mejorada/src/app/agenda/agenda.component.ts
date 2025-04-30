db981c8b4e4d23357625c0cdbbbd127df5889ab5
pendergastt <penddergas@gmail.com>
2025-04-30 13:04:16 +0200

    nuevo ejercicio

---
+++ projects/07_agenda_mejorada/src/app/agenda/agenda.component.ts
@@ -0,0 +1,15 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-agenda',
+  imports: [CommonModule,FormsModule],
+  templateUrl: './agenda.component.html',
+  styleUrl: './agenda.component.css'
+})
+
+export class AgendaComponent {
+
+}
+
---
+++ projects/07_agenda_mejorada/src/app/model/contacto.ts
@@ -0,0 +1,11 @@
+// Lo ponemos en otro archivo aparte porque es posible que tengamos que llamar a este archivo
+// desde otros
+
+export class Contacto{
+
+  nombre:string;
+  apellido:string;
+  telefono:string; //ponemos string porque no vamos a operar con esa variable
+  email:string;
+
+  }
