6922d9aa812a928fc06be0e44b5800be20fa97a0
pendergastt <penddergas@gmail.com>
2025-04-30 11:45:57 +0200

    App notas terminada

---
+++ projects/06_notas/src/app/app.component.ts
@@ -0,0 +1,72 @@
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  imports: [CommonModule, FormsModule],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+
+nota:number;
+listaNotas:number[]=[];
+visible:boolean=false;
+media:number=0;
+masAlta:number;
+masBaja:number;
+aprobados:number;
+
+///////// METODOS /////////
+// Sirven
+
+guardar(){
+  if(this.nota>=0 && this.nota<=10) this.listaNotas.push(this.nota);
+
+  this.hacerMedia();
+  this.notaMayor();
+  this.notaMenor();
+  this.cuantosAprobados();
+
+};
+
+mostrarDatos(){
+  this.visible=!this.visible
+  console.log(this.visible);
+
+};
+
+hacerMedia(){
+  /* Esto está hecho con FOR normal
+  let a:number=0;
+  for(let i of this.listaNotas){
+    a+=i
+  }
+  this.media=a/(this.listaNotas.length);
+  console.log("media es"+this.media);
+  */
+
+  // Lo hacemos con reduce (ponemos a,n como acumulador y N como cada cosa del array)
+  this.media=this.listaNotas.reduce((a,n)=>a+n)/this.listaNotas.length;
+  console.log(this.media);
+
+}
+
+notaMayor(){
+  this.masAlta=Math.max(...this.listaNotas)
+
+}
+
+notaMenor(){
+  this.masBaja=Math.min(...this.listaNotas);
+
+}
+
+cuantosAprobados(){
+  this.aprobados=this.listaNotas.filter((a)=>a>=5).length
+  console.log(this.aprobados+" aprobados");
+
+}
+
+}
