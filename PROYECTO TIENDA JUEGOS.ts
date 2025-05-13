dcd0dfdb4e83855b0f81da50206ef8da30b66e9d
Pendergastt <penddergast@gmail.com>
2025-05-14 00:32:57 +0200

    proyecto tienda juegos

---
+++ PROYECTO TIENDA JUEGOS.ts
@@ -0,0 +1,49 @@
+JUEGOS DE MESA
+
+export class fichaJuego{
+
+    nombre:string;
+    tipo:string;
+    dificultad:number;
+    jugadores:number;
+    duracion:number;
+    imagen:string;
+
+
+}
+
+BASES DE DATOS
+
+ALMACEN
+idProducto
+nombreProducto
+cantidadProducto
+precioProducto
+
+LISTA DE JUEGOS
+nombre
+tipo
+dificultad
+jugadores
+duracion
+imagen
+
+LISTA DE USUARIOS
+idUsuario
+nombreUsuario
+apellidosUsuario
+email
+direccion
+telefono
+numeroCuentaUsuario
+
+CUENTA DE USUARIO (numerocuentausuario)
+fecha
+producto
+importe
+compra/devolucion
+
+
+Necesitamos correo electronico para formulario, consultas etc(googleForms)
+
+
